a6c6e1b4a318792e5ca741d8917e906e55656d5e
SerenTiana <serentiana2288@gmail.com>
2024-11-20 19:29:35 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+let font;
+let tSize = 150;////size of the text
+let tposX = 350;////// X position of text
+let tposY = 500;/////// Y position of text
+let pointCount = 0.9; /// between 0 - 1 particle count
+
+let speed = 10;/// speed of particles
+let comebackSpeed = 100;////// lower numbers less strenght
+let dia = 50;////diameter of interaction
+let randomPos = true;///starting positions - true or false
+let pointsDirection = "general";/////left right up down general 
+let interactionDirection = -1;///// between -1 and 1
+
+let textPoints = [];
+
+function preload() {
+  font = loadFont("AvenirNextLTPro-Demi.otf");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); //fills screen on a wesbsite
+  
+  tposX = width/2 - tSize - 7
+  tposY = height/2 - tSize/ 8
+
+  textFont(font);
+
+  let points = font.textToPoints("STAR", tposX, tposY, tSize, {
+    sampleFactor: pointCount,
+  });
+
+  for (let i = 0; i < points.length; i++) {
+    let pt = points[i];
+
+    let textPoint = new Interact(
+      pt.x,
+      pt.y,
+      speed,
+      dia,
+      randomPos,
+      comebackSpeed,
+      pointsDirection,
+      interactionDirection
+    );
+    textPoints.push(textPoint);
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (let i = 0; i < textPoints.length; i++) {
+    let v = textPoints[i];
+    v.update();
+    v.show();
+    v.behaviors();
+  }
+}
+ 
+
+function windowResized(){
+resizeCanvas(windowWidth, windowHeight)
+}
